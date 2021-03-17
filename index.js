5f98399e8a4642b544e28e51da5aad60bcfd2e1d
Jaime Garcia <jaimegarciaamoros@gmail.com>
2021-03-17 22:21:36 +0100

    add javascript code

--- index.js
+++ index.js
@@ -10,3 +10,81 @@ else if(precio < 30) {
 else {
     console.log('El precio es mayor que 30');
 };
+
+
+
+let sum = 0;
+
+for(let i = 0; i < 10; i = i + 1){
+    sum = sum + i;
+};
+console.log(sum);
+
+
+
+
+let counter = 0;
+
+while(counter < 10){
+    counter = counter + 1;
+};
+console.log(counter);
+
+
+
+
+
+function calcularPrice(a,b){
+    return a + b;
+};
+console.log(calcularPrice(2,3));
+
+
+function createWelcomeMessage(name){
+    const message = 'Welcome to the party ' + name;
+    return message;
+};
+
+const welcomeMessage = createWelcomeMessage('Jaime');
+console.log(welcomeMessage);
+
+
+const despedidaMessage = (nombre) => {
+    return 'Nos vemos pronto ' + nombre;
+};
+console.log(despedidaMessage('Jaime'));
+
+
+
+
+
+// const money = prompt('Cuanto dinero tienes?');
+// const productPrice = 100;
+
+// if(money < productPrice){
+//     console.log('No tienes suficiente dinero para comprar el producto');
+// }
+// else{
+//     const newMoney = money - productPrice;
+//     console.log('Product comprado. Te quedan ' + newMoney + '€');
+// };
+
+
+const prices = [100, 50, 10];
+
+let totalPrice = 0;
+
+for(let i = 0; i < prices.length; i += 1){
+    totalPrice += prices[i];
+};
+console.log(totalPrice);
+
+
+
+function repeatStr (n, s) {
+    for(let i=0;i<n;i+=1){
+      return s;  
+    }
+};
+
+console.log(repeatStr(6,'*'));
